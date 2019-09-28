86e86e9430d390885cf76507b88c038ef346f30c
Randel Jiao <randeljiao@gmail.com>
2019-09-28 20:08:57 +0000

    Automatically backed up by Learn

---
+++ randel.rb
@@ -0,0 +1,8 @@
+def speak_to_grandma("")
+  if "" == "".upercase
+    puts "NO, NOT SINCE 1938!"
+  elsif
+    puts "HUH?! SPEAK UP, SONNY!"
+  else "" == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
