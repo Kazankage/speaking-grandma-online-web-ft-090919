ba7e650e2214f8723404fe73b999b9c4bfad2e18
Randel Jiao <randeljiao@gmail.com>
2019-09-28 20:15:02 +0000

    Automatically backed up by Learn

--- randel.rb
+++ randel.rb
@@ -1,5 +1,5 @@
-def speak_to_grandma("")
-  if "" == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
   if "" != "".upercase
