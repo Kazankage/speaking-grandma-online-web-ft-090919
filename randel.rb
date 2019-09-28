af0320d69e29d273a07205d837dcdeda85c02e79
Randel Jiao <randeljiao@gmail.com>
2019-09-28 20:16:40 +0000

    Automatically backed up by Learn

--- randel.rb
+++ randel.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
   end
-  if phrase != "".uppercase
-    puts "HUH?! SPEAK UP, SONNY!"
-  else 
-    puts "NO, NOT SINCE 1938!"
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
 
